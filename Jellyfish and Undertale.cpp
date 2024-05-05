7c0df772f566ea4d722b2d2a9c0bbcc581f783e5
allanjoseph01 <allanjoseph3005@gmail.com>
2024-05-05 12:59:27 +0530

    Q52

---
+++ Jellyfish and Undertale.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[100];
+int main(){
+	int t;
+	cin >> t;
+	for(int _=0;_<t;_++){
+		int a,b,n;
+		cin >> a >> b >> n;
+		for(int i=0;i<n;i++){
+			cin >> arr[i];
+		}
+		if(a==1){
+			cout << "1" << endl;
+		}else{
+			sort(arr,arr+n);
+			int j=0;
+			long long count=0;
+			while(j<n){
+				if(b==1){
+					b=min(a,b+arr[j]);
+					count+=1;
+					b-=1;
+					j+=1;
+				}else{
+					count+=(b-1);
+					b=1;
+				}
+			}
+			count+=b;
+			cout << count << endl;
+		}
+	}
+}

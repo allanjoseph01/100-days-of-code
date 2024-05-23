5781b1d9809545ae0b6092614cdaf32d8288d071
allanjoseph01 <allanjoseph3005@gmail.com>
2024-05-23 06:25:22 +0530

    Q69

--- Eating Queries.cpp
+++ Eating Queries.cpp
@@ -1,40 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-int lbound(vector<int>are,int tar){
-	int high=are.size()-1;
-	int low=0;
-	int mid;
-	int ans=-1;
-	while(low<=high){
-		mid=(low+high)/2;
-		if(are[mid]<=tar){
-			ans=mid;
-			low=mid+1;
-		}else if(are[mid]>tar){
-			high=mid-1;
-		}
-	}
-	return ans;
-}
-int solve(vector<int> lst,long long s){
-	long long a=s;
-	int count=0;
-	while(true){
-		int bi=lbound(lst,a);
-		a-=lst[bi];
-		count+=1;
-		if(a<=0){
-			return count;
-		}
-		if(lst[bi]==0 || bi==-1){
-			break;
-		}
-		lst[bi]=0;
-		sort(lst.begin(),lst.end());
-	}
-	return -1;
-}
 int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	int t;
 	cin >> t ;
 	for(int _=0;_<t;_++){
@@ -44,12 +12,32 @@ int main(){
 		for(int i=0;i<n;i++){
 			cin >> arr[i];
 		}
-		sort(arr.begin(),arr.end());
+		sort(arr.rbegin(),arr.rend());
+		vector<long long> prefixsum(n);
+		prefixsum[0]=arr[0];
+		for(int i=1;i<n;i++){
+			prefixsum[i]=prefixsum[i-1]+arr[i];
+		}
 		for(int j=0;j<q;j++){
 			long long qu;
 			cin >> qu;
-			vector<int>arr2=arr;
-			cout << solve(arr2,qu) << endl;
+			int high=n;
+			int low=1;
+			int mid;
+			int ans=-1;
+			while(low<=high){
+				mid=(low+high)/2;
+				if(prefixsum[mid-1]==qu){
+					ans=mid;
+					break;
+				}else if(prefixsum[mid-1]>qu){
+					ans=mid;
+					high=mid-1;
+				}else{
+					low=mid+1;
+				}
+			}
+			cout << ans << endl;
 		}
 	}
 }
